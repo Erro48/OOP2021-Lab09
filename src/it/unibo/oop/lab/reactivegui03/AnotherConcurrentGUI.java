1e32693b4bf8147901803fa735340a771095dab3
Enrico <errotagliaferri.4801@gmail.com>
2021-11-25 22:28:12 +0100

    Remove unused imports

--- src/it/unibo/oop/lab/reactivegui03/AnotherConcurrentGUI.java
+++ src/it/unibo/oop/lab/reactivegui03/AnotherConcurrentGUI.java
@@ -2,15 +2,11 @@ package it.unibo.oop.lab.reactivegui03;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 
 public class AnotherConcurrentGUI extends JFrame {
     private static final long serialVersionUID = 1L;
