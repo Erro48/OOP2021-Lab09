3492bc83050e2962653e2ff046ad817c33540fc5
Enrico <errotagliaferri.4801@gmail.com>
2021-11-25 22:01:33 +0100

    Remove everything in AnotherCounter class except constructor and gui modelling

--- src/it/unibo/oop/lab/reactivegui03/AnotherConcurrentGUI.java
+++ src/it/unibo/oop/lab/reactivegui03/AnotherConcurrentGUI.java
@@ -20,7 +20,6 @@ public class AnotherConcurrentGUI extends JFrame {
     private final JButton up = new JButton("up");
     private final JButton down = new JButton("down");
     private final JButton stop = new JButton("stop");
-    private volatile boolean stopCounter;
     
     public AnotherConcurrentGUI() {
         super();
@@ -34,122 +33,6 @@ public class AnotherConcurrentGUI extends JFrame {
         panel.add(stop);
         this.getContentPane().add(panel);
         this.setVisible(true);
-        
-        final Agent agent = new Agent();
-        new Thread(agent).start();
-        final WaitAgent wagent = new WaitAgent();
-        new Thread(wagent).start();
-        
-        stop.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                agent.stopCounting();
-                up.setEnabled(false);
-                down.setEnabled(false);
-                stop.setEnabled(false);
-            }
-        });
-        up.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                agent.increment();
-            }
-        });
-        down.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                agent.decrement();
-            }
-        });
     }
     
-    private class Agent implements Runnable {
-        /*
-         * Stop is volatile to ensure visibility. Look at:
-         * 
-         * http://archive.is/9PU5N - Sections 17.3 and 17.4
-         * 
-         * For more details on how to use volatile:
-         * 
-         * http://archive.is/4lsKW
-         * 
-         */
-        private static final int DELAYMS = 100;
-        private volatile int counter;
-        private boolean countingDirection = true;
-
-        @Override
-        public void run() {
-            while (!AnotherConcurrentGUI.this.stopCounter) {
-                try {
-                    /*
-                     * All the operations on the GUI must be performed by the
-                     * Event-Dispatch Thread (EDT)!
-                     */
-                    SwingUtilities.invokeAndWait(new Runnable() {
-                        @Override
-                        public void run() {
-                            // This will happen in the EDT: since i'm reading counter it needs to be volatile.
-                            AnotherConcurrentGUI.this.display.setText(Integer.toString(Agent.this.counter));
-                        }
-                    });
-                    /*
-                     * SpotBugs shows a warning because the increment of a volatile variable is not atomic,
-                     * so the concurrent access is potentially not safe. In the specific case of this exercise,
-                     * we do synchronization with invokeAndWait, so it can be ignored.
-                     *
-                     * EXERCISE: Can you think of a solution that doesn't require counter to be volatile?
-                     */
-                    if (this.countingDirection) {
-                        this.counter++;
-                    } else {
-                        this.counter--;
-                    }
-                    Thread.sleep(DELAYMS);
-                } catch (InvocationTargetException | InterruptedException ex) {
-                    /*
-                     * This is just a stack trace print, in a real program there
-                     * should be some logging and decent error reporting
-                     */
-                    ex.printStackTrace();
-                }
-            }
-        }
-
-        /**
-         * External command to stop counting.
-         */
-        public void stopCounting() {
-            AnotherConcurrentGUI.this.stopCounter = true;
-        }
-        
-        public void increment() {
-            this.countingDirection = true;
-        }
-        
-        public void decrement() {
-            this.countingDirection = false;
-        }
-    }
-    
-    private class WaitAgent implements Runnable {
-        
-        private static final int DELAY = 10_000;
-        
-        @Override
-        public void run() {
-            while (!AnotherConcurrentGUI.this.stopCounter) {                
-                try {
-                    Thread.sleep(DELAY);
-                    AnotherConcurrentGUI.this.stopCounter = true;
-                    AnotherConcurrentGUI.this.up.setEnabled(false);
-                    AnotherConcurrentGUI.this.down.setEnabled(false);
-                    AnotherConcurrentGUI.this.stop.setEnabled(false);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        
-    }
 }
