603b784de0225f69df93e85ed5149456ce703da2
Enrico <errotagliaferri.4801@gmail.com>
2021-11-26 17:41:53 +0100

    Add constructor and private field nthread to MultiThread class

--- src/it/unibo/oop/lab/workers02/MultiThreadedSumMatrix.java
+++ src/it/unibo/oop/lab/workers02/MultiThreadedSumMatrix.java
@@ -1,6 +1,12 @@
 package it.unibo.oop.lab.workers02;
 
 public class MultiThreadedSumMatrix implements SumMatrix {
+    
+    private final int nthread;
+    
+    public MultiThreadedSumMatrix(final int n) {
+        this.nthread = n;
+    }
 
     @Override
     public double sum(double[][] matrix) {
